34c1def2a9643b760ada41c4521b9d919727432c
blwang59 <blwang59@gmail.com>
2017-04-17 19:33:30 +0800

    IC model 100 times

--- ICmodel.py
+++ ICmodel.py
@@ -1,7 +1,46 @@
 import json
+import random
 
 network = json.load(open('./network_final.json'))
 
 
-def choose_seed(seeds):
-	
+def ICmodel(net,seeds,times):
+
+	net=network
+	fr = open('./ICres.txt', 'w', encoding='utf-8')
+	for i in range(times):
+		target = []
+		active = []
+	# for i in net:
+	# 	for j in net[i]:
+
+	#neighbor_dict = net[seeds]
+	#neighbor_q = neighbor_dict.keys()
+		#fr = open('./ICres.txt', 'w', encoding='utf-8')
+		target.append(seeds)
+		ltimes = 0
+		while(target):
+			ltimes+=1
+			node = target.pop()
+
+			active.append(node)
+			for follower in net[node].keys():
+				if random.random() <= net[node][follower]:
+					if follower not in active:
+						target.append(follower)
+						fr.write(node + ' ' + follower + ' '+str(ltimes)+'\n')
+
+		fr.write('\n')
+
+	fr.close()
+
+
+
+ICmodel(network,"4688",100)
+
+
+
+
+
+
+
--- paper2network.py
+++ paper2network.py
@@ -1,6 +1,6 @@
 # coding:UTF-8
 import codecs
-
+import json
 
 class Node:
     def __init__(self, name, times=0):
@@ -30,7 +30,7 @@ class Graph:
         self.node[self.node.index(name1)][self.node.index(name2)] += 1
 
     def print_net(self):
-        fnet = open('C:/Users/Administrator/Desktop/net.txt', 'w', encoding='utf-16')
+        #fnet = open('C:/Users/Administrator/Desktop/net.txt', 'w', encoding='utf-16')
         for i in range(0, len(self.node)):
             for j in range(0, len(self.node)):
                 fnet.write(str(self.node[i][j]))
@@ -46,18 +46,18 @@ def add_networks(dic, key_a, key_b, val):
     else:
         dic.update({key_a: {key_b: val}})
 # f1 = open('C:/Users/Administrator/Desktop/net.txt', 'w', encoding='utf-16')
-with codecs.open('E:/Aminer/Aminer-Paper.txt', 'r', encoding='utf-16', errors='ignore') as f:
+with codecs.open('./AMiner-Paper.txt', 'r', encoding='utf-8', errors='ignore') as f:
     for line in f:
         # a = line.split(' ')
-        if line[:2] == '#@':
-            authors = line[3:-1].split(';')
-            for author in authors:
-                if author != '':
-                    # if author not in author_dict:
-                        author_dict[author] = 0
+        # if line[:2] == '#@':
+        #     authors = line[3:-1].split(';')
+        #     for author in authors:
+        #         if author != '':
+        #              if author not in author_dict:
+        #                 author_dict[author] = 0
 
-                    # else:
-                        # author_dict[author] += 1
+        #              else:
+        #                  author_dict[author] += 1
         if line[:2] == '#@':
             authors = line[3:-1].split(';')
             if len(authors) > 1:
@@ -66,45 +66,11 @@ with codecs.open('E:/Aminer/Aminer-Paper.txt', 'r', encoding='utf-16', errors='i
                             if author not in network_dict or author2 not in network_dict or author2 not in network_dict[author] or author not in network_dict[author2]:
                                 add_networks(network_dict, author, author2, 1)
                                 add_networks(network_dict, author2, author, 1)
-                                # add_networks(network_dict, author2, author, 1)
-                            # network_dict.update({author:{author2: 1}})
+                                
                             else:
                                 network_dict[author][author2] += 1
                                 network_dict[author2][author] += 1
-                                # network_dict[author2][author] += 1
-                            # print(network_dict.items())
-    # network_dict.fromkeys(author_dict.keys())
-    # for k in network_dict:
-    #     k.fromkeys(author_dict.keys())
-    # for line in f:
-    #     for author in authors:
-    #         for author2 in authors:
-    #             network_dict[author][author2] += 1
 
 
-# print(author_dict.items())
-#
-# print(network_dict.items())
-
-# g = Graph()
-# for author in author_dict.keys():
-#     if author_dict[author] >= 10:
-#         g.add_node_by_name(author, author_dict[author])
-#
-# g.con_net()
-#
-# g.print_net()
-# with codecs.open('E:/Aminer/Aminer-Paper.txt', 'r', encoding='utf-16', errors='ignore') as f:
-#     for lines in f:
-#         if line[:2] == '#@':
-#             authors = line[3:-1].split(';')
-#             for author1 in authors:
-#                 if author1 != '':
-#                     for author2 in authors:
-#                         if author2 != '':
-#                             g.assign_net(author1, author2)
-#
-#
-
-# f1.write(str(network_dict.items()))
-# f1.close()
+fr = open('./network_withsamename.json', 'w',encoding = 'utf-8',errors='ignore')
+json.dump(network_dict,fr)
